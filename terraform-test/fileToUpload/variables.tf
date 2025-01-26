9646ae32c06d493d50867aa9205c3cb6743a5bb2
madhubanti0007 <91464498+madhubanti0007@users.noreply.github.com>
2025-01-26 20:26:32 +0100

    Delete terraform-test directory

--- terraform-test/fileToUpload/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "storage_account_container_name" {
- description = "This is a variable of type string which contains name of the storage account container"
- type        = string
-}
-
-variable "uploaded_file" {
- description = "This is a variable of type string which contains name of the uploaded file in the storage container"
- type        = string
-}
