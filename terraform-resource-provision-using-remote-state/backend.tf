b60a0d5c30ff0dc54d6367760883162ca82fc6ef
madhubanti0007 <91464498+madhubanti0007@users.noreply.github.com>
2025-02-09 10:56:05 +0100

    Update backend.tf

--- terraform-resource-provision-using-remote-state/backend.tf
+++ terraform-resource-provision-using-remote-state/backend.tf
@@ -11,6 +11,9 @@ terraform {
       storage_account_name = "tfstatelfcpe"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
+      # Uncomment below 2 properties when you want to access Azure Storage using Azure AD credential/RBAC and want to use OpenIDConnect protocol to authenticate
+      # use_oidc           = true
+      #use_azuread_auth    = true
   }
 
 }
