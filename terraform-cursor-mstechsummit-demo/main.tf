33300d7c0b3999732011e922785b4948bdc4cebd
madhubanti0007 <91464498+madhubanti0007@users.noreply.github.com>
2025-06-08 11:54:08 +0200

    Committing terraform-cursor-mstechsummit-demo directory (excluding files in .gitignore) to master branch.

--- terraform-cursor-mstechsummit-demo/main.tf
+++ terraform-cursor-mstechsummit-demo/main.tf
@@ -1,40 +1,8 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-cursor-rg"
   location = "East US"
-}
-
-resource "random_string" "storage_suffix" {
-  length  = 6
-  upper   = false
-  special = false
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestor${random_string.storage_suffix.result}"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "example" {
-  name                  = "testcontainer"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
-}
-
-resource "null_resource" "upload_test_file" {
-  provisioner "local-exec" {
-    command = "az storage blob upload --account-name ${azurerm_storage_account.example.name} --container-name ${azurerm_storage_container.example.name} --name testUpload.txt --file ../testUpload.txt --auth-mode key --account-key ${azurerm_storage_account.example.primary_access_key}"
-    interpreter = ["bash", "-c"]
-  }
-  depends_on = [azurerm_storage_container.example]
 } 
--- terraform-cursor-mstechsummit-demo/outputs.tf
+++ terraform-cursor-mstechsummit-demo/outputs.tf
@@ -4,16 +4,4 @@ output "resource_group_name" {
 
 output "resource_group_location" {
   value = azurerm_resource_group.example.location
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.example.name
-}
-
-output "storage_account_primary_endpoint" {
-  value = azurerm_storage_account.example.primary_blob_endpoint
-}
-
-output "storage_container_name" {
-  value = azurerm_storage_container.example.name
 } 
